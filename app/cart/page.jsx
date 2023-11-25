4127fe3e8f68132e96698a26500b98d9b0d10ba6
Paroxyse <failniel04@gmail.com>
2023-11-25 00:38:10 -0600

    Added cart

--- app/cart/page.jsx
+++ app/cart/page.jsx
@@ -1,3 +1,4 @@
+"use client";
 import Image from 'next/image';
 import { useSelector, useDispatch } from 'react-redux';
 // Importing actions from  cart.slice.js
@@ -6,12 +7,21 @@ import {
   decrementQuantity,
   removeFromCart,
 } from '../../redux/cart.slice';
+import store from '../../redux/store';
+import { Provider} from 'react-redux';
 import styles from '../../styles/CartPage.module.css';
 
-const CartPage = () => {
+const CartPage = (props) => {
 
-  const cart = useSelector((state) => state.cart);
+ 
+  return  (<Provider store={store}>
+  <WithProvider {...props} />
+</Provider>);
+  
+};
+function WithProvider(props) {
   const dispatch = useDispatch();
+  const cart = useSelector((state) => state.cart);
 
   const getTotalPrice = () => {
     return cart.reduce(
@@ -19,48 +29,47 @@ const CartPage = () => {
       0
     );
   };
-
   return (
-    <div className={styles.container}>
-      {cart.length === 0 ? (
-        <h1>Your Cart is Empty!</h1>
-      ) : (
-        <>
-          <div className={styles.header}>
-            <div>Image</div>
-            <div>Product</div>
-            <div>Price</div>
-            <div>Quantity</div>
-            <div>Actions</div>
-            <div>Total Price</div>
-          </div>
-          {cart.map((item) => (
-            <div className={styles.body}>
-              <div className={styles.image}>
-                <Image src={item.image} height="90" width="65" />
-              </div>
-              <p>{item.product}</p>
-              <p>$ {item.price}</p>
-              <p>{item.quantity}</p>
-              <div className={styles.buttons}>
-                <button onClick={() => dispatch(incrementQuantity(item.id))}>
-                  +
-                </button>
-                <button onClick={() => dispatch(decrementQuantity(item.id))}>
-                  -
-                </button>
-                <button onClick={() => dispatch(removeFromCart(item.id))}>
-                  x
-                </button>
-              </div>
-              <p>$ {item.quantity * item.price}</p>
+      <div className={styles.container}>
+        {cart.length === 0 ? (
+          <h1>Your Cart is Empty!</h1>
+        ) : (
+          <>
+            <div className={styles.header}>
+              <div>Image</div>
+              <div>Product</div>
+              <div>Price</div>
+              <div>Quantity</div>
+              <div>Actions</div>
+              <div>Total Price</div>
             </div>
-          ))}
-          <h2>Grand Total: $ {getTotalPrice()}</h2>
-        </>
-      )}
-    </div>
+            {cart.map((item) => (
+              <div className={styles.body}>
+                <div className={styles.image}>
+                  <Image src={item.image} height="90" width="65" />
+                </div>
+                <p>{item.product}</p>
+                <p>$ {item.price}</p>
+                <p>{item.quantity}</p>
+                <div className={styles.buttons}>
+                  <button onClick={() => dispatch(incrementQuantity(item.id))}>
+                    +
+                  </button>
+                  <button onClick={() => dispatch(decrementQuantity(item.id))}>
+                    -
+                  </button>
+                  <button onClick={() => dispatch(removeFromCart(item.id))}>
+                    x
+                  </button>
+                </div>
+                <p>$ {item.quantity * item.price}</p>
+              </div>
+            ))}
+            <h2>Grand Total: $ {getTotalPrice()}</h2>
+          </>
+        )}
+      </div>
+    
   );
-};
-
+}
 export default CartPage;
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,27 @@
+"use client"
 import Link from 'next/link';
+import { useSelector } from 'react-redux';
 import styles from '../styles/Navbar.module.css';
-
+import { Provider} from 'react-redux';
+import store from '../redux/store';
 const Navbar = () => {
+return (
+<Provider store={store}>
+<WithProvider />
+</Provider>
+);
+};
+const WithProvider = () => {
+
+  // Selecting cart from global state
+  const cart = useSelector((state) => state.cart);
+
+  // Getting the count of items
+  const getItemsCount = () => {
+    return cart.reduce((accumulator, item) => accumulator + item.quantity, 0);
+  };
+
+  
   return (
     <nav className={styles.navbar}>
       <h6 className={styles.logo}>GamesKart</h6>
@@ -13,11 +33,12 @@ const Navbar = () => {
           <Link href="/shop">Shop</Link>
         </li>
         <li className={styles.navlink}>
-          <Link href="/cart">Cart</Link>
+          <Link href="/cart">
+            <p>Cart ({getItemsCount()})</p>
+          </Link>
         </li>
       </ul>
     </nav>
-  );
+  );;
 };
-
 export default Navbar;
